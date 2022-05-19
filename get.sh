c485db8df7287d8a8cbcca4aa8eef2acc68a17dc
Ramin <ek3nk4r@gmail.com>
2022-05-19 14:25:00 +0430

    x2ee
    x23eee

---
+++ get.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# This is an example of a File Header comments in Bash
+#
+# Copyright 2020 John Doe
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+wget -O /root/MHDDoS/start.py https://github.com/ek3nk4r/KIllME/raw/main/start.py
+wget -O /root/MHDDoS/files/proxies/proxys.txt https://raw.githubusercontent.com/ek3nk4r/KIllME/main/proxys.txt
+cd /root/MHDDoS/
