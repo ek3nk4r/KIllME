b8098f8bdb9da9743ee3e769f7ffedbae5e7395e
Ramin <ek3nk4r@gmail.com>
2022-05-25 11:07:12 +0430

    Update get.sh

--- get.sh
+++ get.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget --no-cache -O /root/MHDDoS/start.py https://raw.githubusercontent.com/ek3nk4r/KIllME/main/start.py
-wget --no-cache -O /root/MHDDoS/start.py https://raw.githubusercontent.com/ek3nk4r/KIllME/main/config.json
+wget --no-cache -O /root/MHDDoS/config.json https://raw.githubusercontent.com/ek3nk4r/KIllME/main/config.json
 wget --no-cache -O /root/MHDDoS/files/proxies/proxys.txt https://raw.githubusercontent.com/ek3nk4r/KIllME/main/proxys.txt
