0ddb9e832e22933eca81ae48b4438318e277a5d8
Ramin <ek3nk4r@gmail.com>
2023-07-24 11:17:58 +0330

    Update get.sh
    upp

--- get.sh
+++ get.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget --no-cache -O /root/MHDDoS/start.py "https://raw.githubusercontent.com/ek3nk4r/KIllME/main/start.py?$(date +%s)"
-wget --no-cache -O /root/MHDDoS/config.json "https://raw.githubusercontent.com/ek3nk4r/KIllME/main/config.json?$(date +%s)"
-wget --no-cache -O /root/MHDDoS/files/proxies/proxys.txt "https://raw.githubusercontent.com/ek3nk4r/KIllME/main/proxys.txt?$(date +%s)"
+curl -f -o /root/MHDDoS/start.py "https://raw.githubusercontent.com/ek3nk4r/KIllME/main/start.py?$(date +%s)"
+curl -f -o /root/MHDDoS/config.json "https://raw.githubusercontent.com/ek3nk4r/KIllME/main/config.json?$(date +%s)"
+curl -f -o /root/MHDDoS/files/proxies/proxys.txt "https://raw.githubusercontent.com/ek3nk4r/KIllME/main/proxys.txt?$(date +%s)"
